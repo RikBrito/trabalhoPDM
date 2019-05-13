9ba73a35ab512dd68d91dcf1ff1d3c9fbeac59aa
RikBrito <riksbrito@gmail.com>
2019-05-13 15:37:24 -0300

    "feature: map and user location"

--- trabalhopdm/trabalhoPDm/App.js
+++ trabalhopdm/trabalhoPDm/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from '.../src/home/index.js';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return Home;
   }
 }
 
@@ -16,6 +13,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
--- trabalhopdm/trabalhoPDm/src/pages/home/index.js
+++ trabalhopdm/trabalhoPDm/src/pages/home/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import { MapView } from 'expo';
+import { View, Text } from 'react-native';
+import { MapView, Location, Permissions } from 'expo';
 
 export default class Home extends React.Component {
+  state = {
+    region: null
+  };
+
+  async componentDidMount() {
+    const region = await this.fetchCurrentLocation();
+    this.setState({ region });
+  }
+
+  fetchCurrentLocation = async () => {
+    const {
+      coords: { latitude, longitude }
+    } = await this.getLocationAsync();
+
+    return {
+      latitude,
+      longitude,
+      latitudeDelta: 0.0143,
+      longitudeDelta: 0.0134
+    };
+  };
+
+  getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') return null;
+
+    return Location.getCurrentPositionAsync({});
+  };
+
   render() {
-    return <MapView showsUserLocation />;
+    const { region } = this.state;
+
+    return <MapView showsUserLocation style={{ flex: 1 }} region={region} />;
   }
 }

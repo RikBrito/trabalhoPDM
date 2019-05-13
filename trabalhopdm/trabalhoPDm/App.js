cabc12d67ac61b68408b6be926263444705dae91
kerherick <50116988+kerherick@users.noreply.github.com>
2019-05-13 14:23:52 -0300

    "home"

--- trabalhopdm/trabalhoPDm/App.js
+++ trabalhopdm/trabalhoPDm/App.js
@@ -3,10 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>aplicativo teste</Text>
-      </View>
+    return (    home
     );
   }
 }

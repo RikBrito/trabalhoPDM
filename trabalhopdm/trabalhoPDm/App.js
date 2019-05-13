fbd1e7b2c75e76a4191e379f177735658f7efddf
kerherick <50116988+kerherick@users.noreply.github.com>
2019-05-13 14:19:00 -0300

    "home"

--- trabalhopdm/trabalhoPDm/App.js
+++ trabalhopdm/trabalhoPDm/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>aplicativo teste </Text>
+        <Text>aplicativo teste</Text>
       </View>
     );
   }

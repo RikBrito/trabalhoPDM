b6d4c9e1c2b0460eccbf5c9ed78a469e563c6b81
kerherick <50116988+kerherick@users.noreply.github.com>
2019-05-16 10:18:57 -0300

    ""

--- trabalhopdm/trabalhoPDm/App.js
+++ trabalhopdm/trabalhoPDm/App.js
@@ -1,5 +1,16 @@
 import React from "react";
 import Home from "./src/pages/home/index.js";
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBowP5XQh221jXixV2p82GMNzX2sZLl4z8",
+  authDomain: "events-cdb1c.firebaseapp.com",
+  databaseURL: "https://events-cdb1c.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  PROJECT_ID:"events-cdb1c",
+  messagingSenderId:""
+};
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
   render() {
